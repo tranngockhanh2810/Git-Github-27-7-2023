313b209c91d4a06b027f1da2b2e4d595c5eafb0b
KhanhAn <tranngockhanh2810@gmail.com>
2023-07-28 11:20:02 +0700

    delete comment

--- src/main.c
+++ src/main.c
@@ -57,13 +57,11 @@ void delay(int rep);
 
 int main(void)
 {
-	RCC->APB2ENR |= 0x10; /// Or  0b10000 --> Anabling Preiph GPIOC
-	GPIOC->CRH &= 0xFF0FFFFF;  /// Reset the PORT C PIN 13
-	GPIOC->CRH |= 0x00300000;  /// Set Port C PIN 13 as Output 
-	GPIOC->ODR  |= 0x2000;     /// Set Port C Pin 13 
-	while(1)
+	RCC->APB2ENR |= 0x10; 
+	GPIOC->CRH &= 0xFF0FFFFF; 
+	GPIOC->ODR  |= 0x2000;     
 	{
-		/// Blinking the Port C pin 13 
+		
 		GPIOC->ODR  |= 0x2000;
 		delay(10);
 		GPIOC->ODR  &= ~0x2000;
